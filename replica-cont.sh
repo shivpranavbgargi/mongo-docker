db17d3a9574614e23c0de087ef2336ca64fc0c0c
Shiv Pranav B Gargi <87698093+shivpranavbgargi@users.noreply.github.com>
2022-03-25 23:35:41 +0530

    Update replica-cont.sh

--- replica-cont.sh
+++ replica-cont.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+mkdir db-repl db-repl2 db-repl3
+
+cp mongod.conf ./db-repl && \
+cp mongod2.conf ./db-repl2 && \
+cp mongod3.conf ./db-repl3
+
 docker run -d --name mongo-repl \
-	-v ~/mongodb/replica-dbs-containers:/data/db/ \
+	-v ./db-repl:/data/db/ \
 	-w /data/db \
 	-p 27017:27017 \
 	mongo:latest bash -c "mongod --config /data/db/mongod.conf" 
 
 docker run -d --name mongo-repl2 \
-	-v ~/mongodb/replica-dbs-containers:/data/db/ \
+	-v ./db-repl2:/data/db/ \
 	-w /data/db \
 	-p 27018:27017 \
 	mongo:latest bash -c "mongod --config /data/db/mongod2.conf"
 
 docker run -d --name mongo-repl3 \
-	-v ~/mongodb/replica-dbs-containers:/data/db/ \
+	-v ./db-repl3:/data/db/ \
 	-w /data/db \
 	-p 27019:27017 \
 	mongo:latest bash -c "mongod --config /data/db/mongod3.conf" 
