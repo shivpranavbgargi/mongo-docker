c3bcb1ba8749eb637243424f1a4ac8ba4b109bab
Shiv Pranav B Gargi <87698093+shivpranavbgargi@users.noreply.github.com>
2022-03-25 23:41:49 +0530

    Update replica.sh

--- replica.sh
+++ replica.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-docker run -it --rm --name mongo \
-	-v ~/mongodb/replica-dbs:/data/db/ \
+docker run -it --rm --name mongo-replica \
+	-v ./db-replica:/data/db/ \
 	-w /data/db \
-	mongo:latest bash -c " mongod --port 27018 --dbpath /data/db/db1 --replSet replica > /dev/null 2>&1 & disown \\
+	mongo:latest bash -c "mongod --port 27018 --dbpath /data/db/db1 --replSet replica > /dev/null 2>&1 & disown \\
 
 mongod --port 27019 --dbpath /data/db/db2 --replSet replica > /dev/null 2>&1 & disown \\
 
 mongod --port 27020 --dbpath /data/db/db3 --replSet replica > /dev/null 2>&1 & sleep 0.5 && bash"
 
-echo "27018 27019 27020"
+cat << EOF 
+
+Ports used: 27018, 27019, 27020
+EOF
