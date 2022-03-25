3225b91586ca9e228c80217409e23cf976977928
Shiva <shivapranavgargi@gmail.com>
2022-03-25 23:59:12 +0530

    Fixed volume mount path

--- replica-cont.sh
+++ replica-cont.sh
@@ -2,24 +2,24 @@
 
 mkdir db-repl db-repl2 db-repl3
 
-cp mongod.conf ./db-repl && \
-cp mongod2.conf ./db-repl2 && \
-cp mongod3.conf ./db-repl3
+cp ./mongod.conf ./db-repl 
+cp ./mongod2.conf ./db-repl2 
+cp ./mongod3.conf ./db-repl3
 
 docker run -d --name mongo-repl \
-	-v ./db-repl:/data/db/ \
+	-v $(pwd):/data/db/ \
 	-w /data/db \
 	-p 27017:27017 \
 	mongo:latest bash -c "mongod --config /data/db/mongod.conf" 
 
 docker run -d --name mongo-repl2 \
-	-v ./db-repl2:/data/db/ \
+	-v $(pwd):/data/db/ \
 	-w /data/db \
 	-p 27018:27017 \
 	mongo:latest bash -c "mongod --config /data/db/mongod2.conf"
 
 docker run -d --name mongo-repl3 \
-	-v ./db-repl3:/data/db/ \
+	-v $(pwd):/data/db/ \
 	-w /data/db \
 	-p 27019:27017 \
 	mongo:latest bash -c "mongod --config /data/db/mongod3.conf" 
