78e521b03ed781b0613dc4ee94c0dbe985537b9b
Shiv Pranav B Gargi <87698093+shivpranavbgargi@users.noreply.github.com>
2022-03-25 23:31:11 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #~/bin/bash
 docker run -it --rm --name mongo \
-	-v ~/mongodb/db:/data/db/ \
+	-v ./db:/data/db/ \
 	-w /data/db \
 	mongo:latest bash -c "mongod > /dev/null 2>&1 & sleep .5 && bash"
