53c099378479d87abd7fd6a12e084c1df620feff
Shiva <shivapranavgargi@gmail.com>
2022-03-27 18:33:45 +0530

    Environment setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-docker network create -d bridge mongonet
+sudo chmod a+x ./run.sh
 
-NETWORK_ID = $(docker inspect -f '{{range.IPAM.Config}}{{.Gateway}}{{end}}' mongonet | sed "s/$//")
-echo ${NETWORK_ID}
+docker network rm _mongonet > /dev/null 2>&1
+docker network create -d bridge _mongonet
+
+NETWORK_ID=$(docker inspect -f '{{range.IPAM.Config}}{{.Gateway}}{{end}}' _mongonet | sed "s/.$//")
+
+sed -i "23s/^.*$/  bindIp: 127.0.0.1,${NETWORK_ID}2/" $PWD/config/mongod.conf
+sed -i "23s/^.*$/  bindIp: 127.0.0.1,${NETWORK_ID}3/" $PWD/config/mongod2.conf
+sed -i "23s/^.*$/  bindIp: 127.0.0.1,${NETWORK_ID}4/" $PWD/config/mongod3.conf
 
