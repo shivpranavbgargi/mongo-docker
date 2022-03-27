00cca7ba71865dcfd86919b84973c68dc76351d9
Shiva <shivapranavgargi@gmail.com>
2022-03-27 23:33:20 +0530

    Changed a variable name
    'NETWORK_ID' to 'network_id'

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ sudo chmod a+x ./run.sh
 docker network rm _mongonet > /dev/null 2>&1
 docker network create -d bridge _mongonet
 
-NETWORK_ID=$(docker inspect -f '{{range.IPAM.Config}}{{.Gateway}}{{end}}' _mongonet | sed "s/.$//")
+network_id=$(docker inspect -f '{{range.IPAM.Config}}{{.Gateway}}{{end}}' _mongonet | sed "s/.$//")
 
-sed -i "23s/^.*$/  bindIp: 127.0.0.1,${NETWORK_ID}2/" $PWD/config/mongod.conf
-sed -i "23s/^.*$/  bindIp: 127.0.0.1,${NETWORK_ID}3/" $PWD/config/mongod2.conf
-sed -i "23s/^.*$/  bindIp: 127.0.0.1,${NETWORK_ID}4/" $PWD/config/mongod3.conf
+sed -i "23s/^.*$/  bindIp: 127.0.0.1,${network_id}2/" $PWD/config/mongod.conf
+sed -i "23s/^.*$/  bindIp: 127.0.0.1,${network_id}3/" $PWD/config/mongod2.conf
+sed -i "23s/^.*$/  bindIp: 127.0.0.1,${network_id}4/" $PWD/config/mongod3.conf
 
